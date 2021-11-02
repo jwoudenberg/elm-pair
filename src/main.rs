8433ae1ef3e979c78cee8be88d27f7c05994cc48
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-11-02 16:56:10 +0100

    Get modified path from event

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate notify;
 
-use notify::{watcher, RecursiveMode, Watcher};
+use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+use std::path::PathBuf;
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
@@ -15,8 +16,29 @@ fn main() {
 
     loop {
         match rx.recv() {
-            Ok(event) => println!("{:?}", event),
+            Ok(event) => handle_event(event),
             Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+}
+
+fn handle_event(event: DebouncedEvent) {
+    println!("{:?}", event_path(event));
+}
+
+fn event_path(event: DebouncedEvent) -> Option<PathBuf> {
+    match event {
+        DebouncedEvent::NoticeWrite(_) => None,
+        DebouncedEvent::NoticeRemove(_) => None,
+        DebouncedEvent::Create(path) => Some(path),
+        DebouncedEvent::Write(path) => Some(path),
+        DebouncedEvent::Chmod(path) => Some(path),
+        DebouncedEvent::Remove(_) => None,
+        DebouncedEvent::Rename(_, path) => Some(path),
+        DebouncedEvent::Rescan => None,
+        DebouncedEvent::Error(e, _) => {
+            println!("error event: {:?}", e);
+            None
         }
     }
 }
