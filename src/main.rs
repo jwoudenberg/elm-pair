e0f1c857efe1ba829403a465d76c6ab73aa23335
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-11-02 16:32:54 +0100

    Add boilerplate file-watching logic
    This is an almost exact copy of the example code in the documentation
    for the notify package.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate notify;
+
+use notify::{watcher, RecursiveMode, Watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, World!");
+    let (tx, rx) = channel();
+
+    let mut watcher = watcher(tx, Duration::from_millis(100)).unwrap();
+
+    watcher
+        .watch("./test-project", RecursiveMode::Recursive)
+        .unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(event) => println!("{:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
 }
