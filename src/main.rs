ff1cbe5bb15bbeb6a02ce6542a20a7ab08c25b5e
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-11-03 12:29:46 +0100

    Use old parse tree in generating the new one
    This doesn't edit the tree before reusing it, so probably this won't
    work as intended. I'll have to look into that with more detail.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ use std::fs;
 use std::path::PathBuf;
 use std::sync::mpsc::channel;
 use std::time::Duration;
+use tree_sitter::Tree;
 
 fn main() {
     let (tx, rx) = channel();
 
     let mut watcher = watcher(tx, Duration::from_millis(100)).unwrap();
+
+    let mut prev_tree = None;
 
     watcher
         .watch("./test-project", RecursiveMode::Recursive)
@@ -17,15 +20,17 @@ fn main() {
 
     loop {
         match rx.recv() {
-            Ok(event) => handle_event(event),
+            Ok(event) => handle_event(&mut prev_tree, event),
             Err(e) => println!("watch error: {:?}", e),
         }
     }
 }
 
-fn handle_event(event: DebouncedEvent) {
-    if let Some(tree) = event_path(event).and_then(parse) {
-        print_tree(0, &mut tree.walk())
+fn handle_event(prev_tree: &mut Option<Tree>, event: DebouncedEvent) {
+    let parse_result = event_path(event).and_then(|path| parse(prev_tree, path));
+    if let Some(tree) = parse_result {
+        print_tree(0, &mut tree.walk());
+        *prev_tree = Some(tree);
     }
 }
 
@@ -46,13 +51,13 @@ fn event_path(event: DebouncedEvent) -> Option<PathBuf> {
     }
 }
 
-fn parse(path: PathBuf) -> Option<tree_sitter::Tree> {
+fn parse(prev_tree: &mut Option<Tree>, path: PathBuf) -> Option<Tree> {
     let code = fs::read(path).ok()?;
     let mut parser = tree_sitter::Parser::new();
     parser
         .set_language(tree_sitter_elm::language())
         .expect("Error loading elm grammer");
-    parser.parse(code, None)
+    parser.parse(code, prev_tree.as_ref())
 }
 
 fn print_tree(indent: usize, cursor: &mut tree_sitter::TreeCursor) {
