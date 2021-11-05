feb48da4868e0fa81462e0259eff66f8e9b33251
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-11-05 14:17:19 +0100

    Handle change events from editor

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,68 @@
-extern crate notify;
-
-use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
 use std::fs;
+use std::io::BufRead;
 use std::path::PathBuf;
-use std::sync::mpsc::channel;
-use std::time::Duration;
-use tree_sitter::Tree;
+use tree_sitter::{InputEdit, Tree};
 
 fn main() {
-    let (tx, rx) = channel();
+    let mut prev_tree: Option<Tree> = None;
 
-    let mut watcher = watcher(tx, Duration::from_millis(100)).unwrap();
-
-    let mut prev_tree = None;
-
-    watcher
-        .watch("./test-project", RecursiveMode::Recursive)
-        .unwrap();
-
-    loop {
-        match rx.recv() {
-            Ok(event) => handle_event(&mut prev_tree, event),
-            Err(e) => println!("watch error: {:?}", e),
-        }
+    let fifo_path = "/tmp/elm-pair";
+    nix::unistd::mkfifo(fifo_path, nix::sys::stat::Mode::S_IRWXU).unwrap();
+    let fifo = std::fs::File::open(fifo_path).unwrap();
+    for line in std::io::BufReader::new(fifo).lines() {
+        let serialized_event = &line.unwrap();
+        let (path, input_edit) = parse_event(serialized_event);
+        handle_event(&mut prev_tree, path, input_edit);
     }
 }
 
-fn handle_event(prev_tree: &mut Option<Tree>, event: DebouncedEvent) {
-    let parse_result = event_path(event).and_then(|path| parse(prev_tree, path));
+fn parse_event(serialized_event: &str) -> (PathBuf, InputEdit) {
+    let (
+        path,
+        start_byte,
+        old_end_byte,
+        new_end_byte,
+        start_row,
+        start_col,
+        old_end_row,
+        old_end_col,
+        new_end_row,
+        new_end_col,
+    ) = serde_json::from_str(serialized_event).unwrap();
+    let start_position = tree_sitter::Point {
+        row: start_row,
+        column: start_col,
+    };
+    let old_end_position = tree_sitter::Point {
+        row: old_end_row,
+        column: old_end_col,
+    };
+    let new_end_position = tree_sitter::Point {
+        row: new_end_row,
+        column: new_end_col,
+    };
+    let input_edit = tree_sitter::InputEdit {
+        start_byte,
+        old_end_byte,
+        new_end_byte,
+        start_position,
+        old_end_position,
+        new_end_position,
+    };
+    (path, input_edit)
+}
+
+fn handle_event(prev_tree: &mut Option<Tree>, path: PathBuf, edit: InputEdit) {
+    if let Some(prev_tree_exists) = prev_tree {
+        prev_tree_exists.edit(&edit);
+    }
+    // TODO: either wait for save event before reading, or get code from editor
+    // directly.
+    let parse_result = parse(prev_tree, path);
     if let Some(tree) = parse_result {
         print_tree(0, &mut tree.walk());
+        println!();
         *prev_tree = Some(tree);
-    }
-}
-
-fn event_path(event: DebouncedEvent) -> Option<PathBuf> {
-    match event {
-        DebouncedEvent::NoticeWrite(_) => None,
-        DebouncedEvent::NoticeRemove(_) => None,
-        DebouncedEvent::Create(path) => Some(path),
-        DebouncedEvent::Write(path) => Some(path),
-        DebouncedEvent::Chmod(path) => Some(path),
-        DebouncedEvent::Remove(_) => None,
-        DebouncedEvent::Rename(_, path) => Some(path),
-        DebouncedEvent::Rescan => None,
-        DebouncedEvent::Error(e, _) => {
-            println!("error event: {:?}", e);
-            None
-        }
     }
 }
 
@@ -61,7 +76,12 @@ fn parse(prev_tree: &mut Option<Tree>, path: PathBuf) -> Option<Tree> {
 }
 
 fn print_tree(indent: usize, cursor: &mut tree_sitter::TreeCursor) {
-    println!("{}{:?}", "  ".repeat(indent), cursor.node());
+    let node = cursor.node();
+    if node.has_changes() {
+        println!("{}CHANGED: {:?}", "  ".repeat(indent), cursor.node());
+    } else {
+        println!("{}{:?}", "  ".repeat(indent), cursor.node());
+    }
     if cursor.goto_first_child() {
         print_tree(indent + 1, cursor);
         cursor.goto_parent();
