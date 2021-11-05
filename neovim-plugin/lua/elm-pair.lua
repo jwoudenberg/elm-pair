7c8125178f0c685ab97528ed151d483785c06e32
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-11-05 14:36:29 +0100

    Run autoformatter on lua code

--- neovim-plugin/lua/elm-pair.lua
+++ neovim-plugin/lua/elm-pair.lua
@@ -1,28 +1,24 @@
 local function start()
-  socket = assert(io.open("/tmp/elm-pair", "a"))
+    socket = assert(io.open("/tmp/elm-pair", "a"))
 
-  local function on_bytes(_, bufnr, changed_tick, start_row, start_col, start_byte, old_row, old_col, old_byte, new_row, new_col, new_byte)
-    local old_end_col = old_col + ((old_row == 0) and start_col or 0)
-    local new_end_col = new_col + ((new_row == 0) and start_col or 0)
-    local filename = vim.fn.expand("%:p")
-    local msg = {
-      filename,
-      start_byte,
-      (start_byte+old_byte),
-      (start_byte+new_byte),
-      start_row,
-      start_col,
-      (start_row+old_row),
-      old_end_col,
-      (start_row+new_row),
-      new_end_col,
-    }
-    socket:write(vim.fn.json_encode(msg))
-    socket:write("\n")
-    socket:flush()
-  end
+    local function on_bytes(_, bufnr, changed_tick, start_row, start_col,
+                            start_byte, old_row, old_col, old_byte, new_row,
+                            new_col, new_byte)
+        local old_end_col = old_col + ((old_row == 0) and start_col or 0)
+        local new_end_col = new_col + ((new_row == 0) and start_col or 0)
+        local filename = vim.fn.expand("%:p")
+        local msg = {
+            filename, start_byte, (start_byte + old_byte),
+            (start_byte + new_byte), start_row, start_col,
+            (start_row + old_row), old_end_col, (start_row + new_row),
+            new_end_col
+        }
+        socket:write(vim.fn.json_encode(msg))
+        socket:write("\n")
+        socket:flush()
+    end
 
-  vim.api.nvim_buf_attach(0, false, {on_bytes=on_bytes})
+    vim.api.nvim_buf_attach(0, false, {on_bytes = on_bytes})
 end
 
-return { start = start, }
+return {start = start}
