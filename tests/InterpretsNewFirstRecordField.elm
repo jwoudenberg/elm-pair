9e5e78ed3ec2ba83cf986716cb56f0d12639b589
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-11-20 21:43:59 +0100

    Add two tests for field addition

---
+++ tests/InterpretsNewFirstRecordField.elm
@@ -0,0 +1,15 @@
+module Bulb exposing (..)
+
+type alias Bulb =
+  { kind : String
+  }
+
+-- START SIMULATION
+-- COMPILATION SUCCEEDS
+-- MOVE CURSOR TO LINE 4 kind
+-- INSERT wattage : Int ,
+-- END SIMULATION
+
+
+-- === expected output below ===
+-- Some(FieldAdded("wattage : Int"))
