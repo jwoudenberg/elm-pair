b41c96d0ce3cc2734891ba2da55eed4166bf7de8
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-12-06 18:11:32 +0100

    Neovim plugin attaches to each Elm file

--- neovim-plugin/lua/elm-pair.lua
+++ neovim-plugin/lua/elm-pair.lua
@@ -1,10 +1,21 @@
 -- luacheck: read globals vim
-local function start()
-    local channel = vim.fn.sockconnect("pipe", "/tmp/elm-pair.sock",
-                                       {rpc = true})
+function _G.elm_pair_on_elm_buffer()
     local buffer = vim.fn.bufnr("%")
     local path = vim.fn.expand("%:p")
-    vim.fn.rpcnotify(channel, "buffer_opened", buffer, path)
+    vim.fn.rpcnotify(_G.elm_pair_channel, "buffer_opened", buffer, path)
+end
+
+local function start()
+    _G.elm_pair_channel = vim.fn.sockconnect("pipe", "/tmp/elm-pair.sock",
+                                             {rpc = true})
+
+    _G.elm_pair_on_elm_buffer()
+
+    vim.cmd([[
+      augroup elm-pair
+        autocmd FileType elm call v:lua.elm_pair_on_elm_buffer()
+      augroup END
+    ]])
 end
 
 return {start = start}
