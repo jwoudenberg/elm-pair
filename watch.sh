e30b01d9ef3122f9d267f20bf73a07a5660aae83
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-12-09 12:02:11 +0100

    watch.sh reloads upon changes in any source file
    This is nicer when writing tests, because a change to a non-rust test
    file will cause tests to rerun.

--- watch.sh
+++ watch.sh
@@ -3,4 +3,4 @@
 
 # Watch source files and recompile when any change.
 
-git ls-files | grep .rs | entr -c -s "cargo clippy --tests && cargo test $1"
+git ls-files | entr -c -s "cargo clippy --tests && cargo test $1"
