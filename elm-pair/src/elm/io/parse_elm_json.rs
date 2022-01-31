80a7b6fa3f76e0a6f0ca2a9f1e80db21d247d430
Jasper Woudenberg <mail@jasperwoudenberg.com>
2022-01-31 20:54:14 +0100

    Support parsing package elm-json files
    These don't have a source_directories property.
    
    Addresses #7.

--- elm-pair/src/elm/io/parse_elm_json.rs
+++ elm-pair/src/elm/io/parse_elm_json.rs
@@ -25,9 +25,87 @@ pub fn parse_elm_json(path: &Path) -> Result<ElmJson, Error> {
     Ok(elm_json)
 }
 
-#[derive(Clone, Deserialize)]
+#[derive(Clone, Debug, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "kebab-case")]
 pub struct ElmJson {
-    #[serde(rename = "source-directories")]
+    #[serde(rename = "source-directories", default = "default_source_dirs")]
     pub source_directories: Vec<PathBuf>,
+}
+
+fn default_source_dirs() -> Vec<PathBuf> {
+    vec![PathBuf::from("src")]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn application_elm_json() {
+        let dir = tempdir::TempDir::new("elm-pair-tests").unwrap().into_path();
+        let elm_json = r#"
+            {
+                "type": "application",
+                "source-directories": [
+                    "src",
+                    "lib/more-src"
+                ],
+                "elm-version": "0.19.1",
+                "dependencies": {
+                    "direct": {
+                        "elm/core": "1.0.5",
+                        "elm/json": "1.1.3",
+                        "elm/parser": "1.1.0",
+                        "elm/time": "1.0.0"
+                    },
+                    "indirect": {}
+                },
+                "test-dependencies": {
+                    "direct": {},
+                    "indirect": {}
+                }
+            }
+        "#;
+        let path = dir.join("elm.json");
+        std::fs::write(&path, elm_json).unwrap();
+        assert_eq!(
+            parse_elm_json(&path),
+            Ok(ElmJson {
+                source_directories: vec![
+                    dir.join("src"),
+                    dir.join("lib/more-src"),
+                ]
+            }),
+        );
+    }
+
+    #[test]
+    fn package_elm_json() {
+        let dir = tempdir::TempDir::new("elm-pair-tests").unwrap().into_path();
+        let elm_json = r#"
+            {
+                "type": "package",
+                "name": "jwoudenberg/elm-pair-experiment",
+                "summary": "If a package exists in a test suite where no-one can install it, does it make a sound?",
+                "license": "MIT",
+                "version": "1.2.3",
+                "exposed-modules": [
+                    "Experiment"
+                ],
+                "elm-version": "0.19.0 <= v < 0.20.0",
+                "dependencies": {
+                    "elm/core": "1.0.0 <= v < 2.0.0"
+                },
+                "test-dependencies": {}
+            }
+        "#;
+        let path = dir.join("elm.json");
+        std::fs::write(&path, elm_json).unwrap();
+        assert_eq!(
+            parse_elm_json(&path),
+            Ok(ElmJson {
+                source_directories: vec![dir.join("src"),]
+            }),
+        );
+    }
 }
